19302079833a9efa1181a4fe219fc99c31bd6939
Rakesh Kushwaha <rk7518329420@gmail.com>
2025-07-28 06:58:39 +0530

    commit

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,5 +1,4 @@
 
-import React from 'react';
 
 export const UploadIcon = ({ className = 'w-6 h-6' }: { className?: string }) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
